989fcb3b8813a6c978e06b949c2cf3f7650d8e42
builderjer <builderjer@gmail.com>
2024-06-09 10:18:35 -0600

    fix test

--- test/plugin_tests.py
+++ test/plugin_tests.py
@@ -6,7 +6,7 @@ from ovos_plugin_manager.phal import find_phal_plugins
 class TestPlugin(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.skill_id = "ovos-PHAL-plugin-hotkeys.builderjer"
+        self.skill_id = "ovos-PHAL-plugin-hotkeys"
 
     def test_find_plugin(self):
         plugins = find_phal_plugins()
