78ac89436ffc24f46f3d57d14ef4662f2c17a83b
miro <jarbasai@mailfence.com>
2024-09-10 01:04:24 +0100

    chore:automations

--- setup.py
+++ setup.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python3
+import os
+from os.path import join
+
 from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+def get_version():
+    """ Find the version of this package"""
+    version_file = join(BASEDIR, 'ovos_PHAL_plugin_mk2_v6_fan_control/version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
 
 
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-hotkeys=ovos_phal_plugin_hotkeys:HotKeysPlugin'
 setup(
     name='ovos-PHAL-plugin-hotkeys',
-    version='0.0.1',
+    version=get_version(),
     description='map keypresses to OVOS bus events',
     url='https://github.com/OpenVoiceOS/ovos-PHAL-plugin-hotkeys',
     author='JarbasAi',
@@ -13,7 +53,7 @@ setup(
     license='Apache-2.0',
     packages=['ovos_phal_plugin_hotkeys',
               'ovos_phal_plugin_hotkeys.keyboard'],
-    install_requires=["ovos-plugin-manager"],
+    install_requires=required("requirements.txt"),
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
